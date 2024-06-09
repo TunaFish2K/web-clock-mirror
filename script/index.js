9281fe77b042bcbcac4c1860839192d2b2566ff3
Tom Lin <lyt080529@163.com>
2024-06-09 19:06:30 +0800

    add loading

--- script/index.js
+++ script/index.js
@@ -1,14 +1,23 @@
-// @ts-check
-
 import { Clock } from "./clock.js";
 import { localize } from "./localization.js"
 import { render } from "./render.js";
-import { loadAll as loadALlResources } from "./resource.js";
+import { loadAll as loadAllResources } from "./resource.js";
 import { init as initSettings } from "./settings.js";
 import { start as startCursorHide } from "./cursor.js";
 
+/**
+ * 
+ * @param { number } ms
+ * @return { Promise<void> } 
+ */
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+
 window.addEventListener("load", async () => {
-    await loadALlResources();
+    await loadAllResources();
 
     globalThis.Clock = Clock;
     Clock.load();
@@ -18,6 +27,7 @@ window.addEventListener("load", async () => {
     startCursorHide();
 
     setInterval(render, 200);
+    document.getElementById("content").hidden=false;
 });
 
 window.onbeforeunload = () => {
